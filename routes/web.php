7354aef7a8426b42f8dc02c42c8abc19f9dd86b8
arallel <mdvitoah@gmail.com>
2022-03-14 13:09:22 +0700

    fix dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
     return view('auth.login');
 });
 // Route::get('test', Test::class);
-// Route::get('dashboard', Users::class);
 Route::get('datadriver', Datadriver::class);
 Route::get('cars', kendaraan::class);
 //route ketika sudah fix
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     });
-    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', Users::class);
-    //     return view('dashboard');
-    // })->name('dashboard');
+
+    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
