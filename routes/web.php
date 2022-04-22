2f48a5561492fa39dc256e3001de43edaac5edc8
m.dien_vito <mdvitoah@gmail.com>
2022-04-22 15:39:07 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ use App\Http\Livewire\Datadriver\Driver;
 use App\Http\Livewire\laporan\Datalaporan;
 use App\Http\Controllers\lapor;
 use App\Http\Controllers\Laporanuser;
+use App\Http\Controllers\konfirmasicontroller;
 use App\Http\Controllers\UserProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,26 +21,37 @@ use App\Http\Controllers\UserProfileController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 */
-Route::get('Dashboard','App\Http\Controllers\homecontroller@index');
+
 Route::get('/', function () {
     return view('auth.login');
 });
+<<<<<<< Updated upstream
 Route::resource('cars', Kendaraan::class);
+=======
+Route::resource('peminjaman',konfirmasicontroller::class);
+Route::get('cars', Kendaraan::class);
+>>>>>>> Stashed changes
 //route ketika sudah fix
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('homepage',function(){
         return view('home');
     }); 
-    Route::get('test',function(){
-    return view('test');
-    });
+    // Route::get('test',function(){
+    // return view('test');
+    // });
     Route::resource('laporanuser',Laporanuser::class);   
+<<<<<<< Updated upstream
     
         Route::resource('datadriver', Driver::class);
         Route::resource('laporan', Datalaporan::class);
+=======
+        Route::get('datadriver', Driver::class);
+        Route::get('laporan', Datalaporan::class);
+>>>>>>> Stashed changes
         Route::get('Laporanuser', function () {
         return view('homepage.lapor');
     });
+    Route::get('Dashboard','App\Http\Controllers\homecontroller@index');
 });
 // Route::get('/profile/photo', UserProfileController::class,'photo');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
